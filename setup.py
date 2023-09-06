04dc9583b48f720558c6e465ac6c4656d39e09ef
90df <90df@users.noreply.github.com>
2023-09-05 22:02:54 -0400

    Remove classifiers

--- setup.py
+++ setup.py
@@ -26,7 +26,5 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta",
     ],
 )
