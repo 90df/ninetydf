167405b9c9d21e7a8853bb317f673a6c2bb1a0e4
90df <144079200+90df@users.noreply.github.com>
2023-09-17 21:01:10 -0400

    v0.5.0 (#5)
    * Add dataclasses, make pandas optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 # Changelog
 
+## 0.5.0 (2023-09-17)
+
+- Fix `michael_juliana` data
+- Add test assertion for couple name format
+- Add `Couple` and `Season` dataclasses
+- Make pandas an optional dependency
+- Add `couples_list` and `seasons_list`
+- Rename `couples` and `seasons` to `couples_df` and `seasons_df`
+
 ## 0.4.0 (2023-09-16)
 
 - Added HEA data
--- src/ninetydf/__init__.py
+++ src/ninetydf/__init__.py
@@ -1,4 +1,12 @@
-from .data import _load_couples, _load_seasons
+from .csv_loader import load_couples, load_seasons
 
-seasons = _load_seasons()
-couples = _load_couples()
+couples_list = load_couples()
+seasons_list = load_seasons()
+
+try:
+    from .df_loader import load_couples_df, load_seasons_df
+
+    couples_df = load_couples_df()
+    seasons_df = load_seasons_df()
+except ImportError:
+    pass
